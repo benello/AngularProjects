5bff5501dc4105b803f4f6f63d3c603eee25b9af
benello <13behnjaminbonello@gmail.com>
2022-07-23 11:24:19 +0200

    Added a navbar to the project homepage

--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeInfoComponent } from './recipes/recipe-info/recipe-info.component';
+import { RecipeComponent } from './recipes/recipe-list/recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { EditComponent } from './shopping-list/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeInfoComponent,
+    RecipeComponent,
+    ShoppingListComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule
