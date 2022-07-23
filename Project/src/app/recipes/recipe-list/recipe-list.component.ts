59b75d696f722534b0468908e63a59068958746b
benello <13behnjaminbonello@gmail.com>
2022-07-23 12:25:17 +0200

    Added functionality to show recipes

--- Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.modal";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipes', 'This is for testing purposes', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png'),
+    new Recipe('Test Recipes 2', 'This is for testing purposes 2', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png')
+
+  ];
 
   constructor() { }
 
