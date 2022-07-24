4725cc424989cf1a212e4219cb83192fe17a331d
benello <13behnjaminbonello@gmail.com>
2022-07-24 12:59:52 +0200

    Added mutliple examples of property & event binding

--- cmp-databinding/src/app/app.component.ts
+++ cmp-databinding/src/app/app.component.ts
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{ type: 'server', name: 'testServer', content: 'just a test'}, {type: 'server', name: 'testServer2', content: 'just a test2'}];
+  serverElements: {type: string, name: string, content: string}[] = [];
 
+  onServerAdded(serverData: {name: string, content: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.name,
+      content: serverData.content
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {name: string, content: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.name,
+      content: blueprintData.content
+    });
+  }
+
+  OnChangeFirst(){
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  OnDestroy(){
+    this.serverElements.splice(0, 1)
+  }
 }
--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,16 +1,63 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  SimpleChanges,
+  OnInit,
+  ViewEncapsulation,
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild
+
+} from '@angular/core';
+import {Element} from "@angular/compiler";
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
-  @Input() element: any;
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input('srvElement') element: any;
+  @Input() name!: string;
+  @ViewChild('heading', {static: true}) header!: ElementRef;
+  @ContentChild('contentP', {static: true}) paragraph!: ElementRef;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('constructor called');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngChanges called')
+  }
+
+  ngOnInit(): void {
+    console.log('nginit called');
+  }
+
+  ngDoCheck(){
+    console.log('ngdoCheck called');
+  }
+
+  ngAfterContentInit(){
+    console.log('aftercontentInit called');
+  }
+  ngAfterContentChecked(){
+    console.log('aftercontentInit checked');
+  }
+
+  ngAfterViewInit() {
+    console.log('after viewinit called');
+    console.log('Text Content of header: '+this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: '+this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log('afterviewchecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('destroyed called');
+  }
 }
